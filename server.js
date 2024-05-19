8cfba0c7913d521f1d23d9ca65c4473cbd66d602
Eden Levin <eluif4@gmail.com>
2024-05-19 22:34:05 +0300

    Global cors

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://localhost:5173"]
+        origin: "*",
     })
 )
 app.use(DBGETARTICLESROUTES)
