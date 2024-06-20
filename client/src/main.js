e4f32f99dbc8068ab6422f0ab2723238f02706d8
Eden Levin <eluif4@gmail.com>
2024-06-20 17:15:41 +0300

    Update manifest.json and service-worker.js

--- client/src/main.js
+++ client/src/main.js
@@ -64,13 +64,13 @@ export const PopupAttributes = reactive({
 
 // ----- SERVICE WORKER -----
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(error => {
-      console.log('ServiceWorker registration failed: ', error);
-    });
+  // window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(error => {
+    console.log('ServiceWorker registration failed: ', error);
   });
+  // });
 }
 
 app.use(router);
