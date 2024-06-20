05fb6336165aaaf9cf49bcac57ec9fe76e87975e
Eden Levin <eluif4@gmail.com>
2024-06-20 14:27:20 +0300

    Fix prod issues

--- client/src/main.js
+++ client/src/main.js
@@ -65,7 +65,7 @@ export const PopupAttributes = reactive({
 // ----- SERVICE WORKER -----
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('./client/service-worker.js').then(registration => {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(error => {
       console.log('ServiceWorker registration failed: ', error);
