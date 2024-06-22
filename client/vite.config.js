0b69ec3a3d2e8dd66160d2cbad09a814b5c31070
Eden Levin <eluif4@gmail.com>
2024-06-22 14:35:07 +0300

    Fix prod issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,9 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { VitePluginStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePluginStaticCopy({
-      targets: [
-        {
-          src: 'favicon/*',
-          dest: 'assets'
-        },
-        {
-          src: 'service-worker.js',
-          dest: '.'
-        }
-      ]
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -38,5 +23,6 @@ export default defineConfig({
         entryFileNames: 'assets/[name].[hash].js'
       }
     }
-  }
+  },
+  publicDir: 'favicon' // Ensures the favicon folder is included in the build
 });
