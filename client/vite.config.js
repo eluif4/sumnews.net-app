3425c2cd6e01f265cc3009d22f945cffa3762a31
Eden Levin <eluif4@gmail.com>
2024-06-22 14:13:25 +0300

    Fix prod issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
-// import mkcert from 'vite-plugin-mkcert'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), 
-    // mkcert()
-  ],
-  // server: { https: true },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        serviceWorker: fileURLToPath(new URL('./service-worker.js', import.meta.url))
+      },
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
+  publicDir: 'favicon' // Ensures the favicon folder is included in the build
+});
