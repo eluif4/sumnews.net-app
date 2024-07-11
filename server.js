0c9eafaf368d3252f5ad630b87708723d6179f01
Eden Levin <eluif4@gmail.com>
2024-07-11 21:40:21 +0200

    activate backend

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ const { articleQueue } = require('./server/2-utils/articleQueueHandler.js');
 //---RUN MAIN FUNCTION---
 async function cronTask() {
     cron.schedule('*/15 * * * *', async () => {
-        if (false) {
+        if (true) {
             try {
                 const date = new Date()
                 console.log(kleur.bgBlue(`Task started @ ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`))
