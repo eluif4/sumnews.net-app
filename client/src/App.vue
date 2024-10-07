023351ef75b4774f275f6a45c564d73652cabfd2
Eden Levin <eluif4@gmail.com>
2024-10-07 17:09:16 +0300

    Remove Desktop Page for Google OAuth Verification

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,8 +10,9 @@ const isDesktop = computed(() => {
 
 <template>
   <div id="app">
-    <DesktopPage v-if="isDesktop"></DesktopPage>
-    <MobilePage v-else></MobilePage>
+    <!-- <DesktopPage v-if="isDesktop"></DesktopPage> -->
+    <!-- <MobilePage v-else></MobilePage> -->
+     <MobilePage></MobilePage>
   </div>
 </template>
 
