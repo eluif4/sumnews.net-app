efa8abe78d76f7c78e97a1eb8e5b53c2e27421d9
Eden Levin <eluif4@gmail.com>
2024-10-18 16:45:21 +0300

    Rebuild backend

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ const { articleQueue } = require('./server/2-utils/articleQueueHandler.js');
 const state = 0;
 async function cronTask() {
     cron.schedule('*/20 * * * *', async () => {
-        if (false) {
+        if (true) {
             try {
                 const date = new Date()
                 console.log(kleur.bgBlue(`Task started @ ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`))
