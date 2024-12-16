d6b6e4f34bf56c9537410c4b085fc25193eb93fe
eluif4 <eluif4@gmail.com>
2024-12-16 16:06:55 +0200

    Trying to correct a wrong merge conflict

--- client/src/components/Article/ArticleContent.vue
+++ client/src/components/Article/ArticleContent.vue
@@ -270,26 +270,6 @@ const openModal = () => {
 </script>
 
 <template>
-    <!-- <button class="btn" onclick="">open modal</button> -->
-    <dialog id="my_modal_5" class="modal">
-        <div class="modal-box">
-            <h3 class="text-lg font-bold">Hello!</h3>
-            <p class="py-4">Press ESC key or click the button below to close</p>
-            <div class="loader" v-if="displayModalLoader">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="50" height="50"><radialGradient id="a10" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)"><stop offset="0" stop-color="#FF156D"></stop><stop offset=".3" stop-color="#FF156D" stop-opacity=".9"></stop><stop offset=".6" stop-color="#FF156D" stop-opacity=".6"></stop><stop offset=".8" stop-color="#FF156D" stop-opacity=".3"></stop><stop offset="1" stop-color="#FF156D" stop-opacity="0"></stop></radialGradient><circle transform-origin="center" fill="none" stroke="url(#a10)" stroke-width="15" stroke-linecap="round" stroke-dasharray="200 1000" stroke-dashoffset="0" cx="100" cy="100" r="70"><animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2" values="360;0" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform></circle><circle transform-origin="center" fill="none" opacity=".2" stroke="#FF156D" stroke-width="15" stroke-linecap="round" cx="100" cy="100" r="70"></circle></svg>
-            </div>
-            <div class="modal-action">
-                <form method="dialog">
-                    <!-- if there is a button in form, it will close the modal -->
-                    <button class="btn">Close</button>
-                </form>
-                <form method="dialog2">
-                    <div class="btn" @click="postArticleToInstagram">Post Article</div>
-                </form>
-            </div>
-        </div>
-    </dialog>
-    <!-- FUTURE CHANGE: add animation into routes -->
     <div class="article-container" id="article-container" :style="{ transform: `translateY(${translateY}px)` }"
         @transitionend="handleTransitionEnd">
         <dialog id="my_modal_5" class="modal">
@@ -409,7 +389,8 @@ const openModal = () => {
             <div class="fullcoverage-container" v-if="aggregatedResults.length == 0 && articleRef.eventUri">
                 <!-- <div class="skeleton h-32 w-full article-instance"></div> -->
                 <p class="skeleton h-8 w-40"></p>
-                <div class="skeleton h-8 w-full fullcoverage-article" v-for="(item, index) in [1, 2, 3, 4, 5]" :key="index"></div>
+                <div class="skeleton h-8 w-full fullcoverage-article" v-for="(item, index) in [1, 2, 3, 4, 5]"
+                    :key="index"></div>
             </div>
 
             <!-- FUTURE CHANGE: add ask ai feature with resources -->
