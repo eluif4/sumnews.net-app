2bdbef092bd29e21cf6d8bce93f9d44d2df8b2ef
Eden Levin <eluif4@gmail.com>
2025-01-03 15:54:23 +0200

    Trying to fix: allows cors for hybrid mobile apps

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(cors({
-    origin: ['https://app.sumnews.net', 'http://localhost', 'http://localhost:5173', 'https:localhost', 'capacitor://localhost', 'ionic://localhost'],
+    origin: ['https://app.sumnews.net', 'http://localhost', 'https://localhost', 'http://localhost:5173',  'capacitor://localhost', 'ionic://localhost'],
     credentials: true, // Allows cookies to be included in requests (if necessary)
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'], // Include 'Cache-Control' here
